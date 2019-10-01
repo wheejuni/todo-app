69189f93343bad9a5a5344ba5c941406d07b42b2
wheejuni <wheejuni@gmail.com>
2019-10-01 22:34:40 +0900

    add Mainpage

---
+++ components/Mainpage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export default class Mainpage extends React.Component {
+    render() {
+        return(
+            <View>
+                <h1>this is main page.</h1>
+            </View>
+        )
+    }
+}
